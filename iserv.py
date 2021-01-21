c0a1041da8f83435aeac4da20b24f6535f0733e0
slogemann1 <sllogemann1@gmail.com>
2021-01-21 16:43:47 +0100

    Added uploading images

--- iserv.py
+++ iserv.py
@@ -3,6 +3,10 @@ import requests
 import random
 import csv
 import re
+from PIL import Image
+import random
+import colorsys
+from io import BytesIO
 
 # Read files
 logins_csv = open("logins.txt")
@@ -11,12 +15,16 @@ logins = csv.reader(logins_csv, delimiter=",")
 birthday_file = open("birthday_payload.txt")
 birthday_payload = birthday_file.read()
 
+hue_file = open("hue.txt")
+hue = int(hue_file.read())
+
 def main():
     for row in logins:
         if len(row) < 2:
             continue
         else:
             change_birthday(row[0], row[1])
+    change_hue()
     return
 
 
@@ -61,7 +69,7 @@ def change_birthday(username, password):
 
     # Send Post Request to Change
     change_url = "https://fkggoettingen.de/iserv/profile/public/edit"
-    change_data = birthday_payload.replace("YOUR_BIRTHDAY_HERE", birthday).replace("YOUR_TOKEN_HERE", token)
+    change_data = create_payload(token, birthday)
     change_headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Connection": "keep-alive",
@@ -70,7 +78,36 @@ def change_birthday(username, password):
     }
 
     change_response = session.post(url=change_url, data=change_data, headers=change_headers)
+    
     return
+
+def create_payload(token, birthday):
+    # Get Color for Image
+    hue_input = hue / 256.0
+    color = colorsys.hsv_to_rgb(hue_input, 1.0, 0.75)
+    color = (int(color[0] * 256), int(color[1] * 256), int(color[2] * 256))
+
+    # Create Image
+    img = Image.new("RGB", (256, 256), color)
+    buffer = BytesIO()
+    img.save(buffer, format="png")
+    image_data = bytes(buffer.getbuffer())
+
+    # Create Payload
+    payload = birthday_payload.replace("YOUR_BIRTHDAY_HERE", birthday).replace("YOUR_TOKEN_HERE", token)
+    payload = payload.split("YOUR_IMAGE_HERE")
+    payload_1 = payload[0]
+    payload_2 = payload[1]
+    payload = bytearray(payload_1, "utf-8")
+    payload.extend(image_data)
+    payload.extend(bytes(payload_2, "utf-8"))
+
+    return payload
+
+def change_hue():
+    new_hue = hue + 10 % 255
+    hue_file = open("hue.txt", "w+")
+    hue_file.write(str(new_hue))
 
 if __name__ == "__main__":
     main()
